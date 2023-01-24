48fb98ea4576aef44d4d93e72e443e61da64e90d
YaKartoshka <100137723+YaKartoshka@users.noreply.github.com>
2023-01-24 14:07:57 +0600

    Create server.js

---
+++ twilio_whatsapp_integration/server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const express = require('express');
+const { urlencoded } = require('body-parser');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const app = express();
+app.use(urlencoded({ extended: false }));
+
+app.get('/',(req,res)=>{
+  res.send("hello")
+})
+
+app.post('/', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  if (req.body.Body == 'hello') {
+    twiml.message('Hi!');
+  } else if (req.body.Body == 'bye') {
+    twiml.message('Goodbye');
+  } else {
+    twiml.message(
+      'No Body param match, Twilio sends this in the request to your server.'
+    );
+  }
+
+  res.type('text/xml').send(twiml.toString());
+});
+
+http.createServer(app).listen(3000, () => {
+  console.log('Express server listening on port 3000');
+});
