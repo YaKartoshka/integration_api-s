e4e3c3c5cfca60bc056dda937d4e5c9e4e32d0ba
YaKartoshka <100137723+YaKartoshka@users.noreply.github.com>
2023-04-18 14:40:31 +0600

    awd

--- instagram_messenger_api/server.js
+++ instagram_messenger_api/server.js
@@ -1,20 +1,45 @@
-const token = "EAATYsedF4CYBABuu8QLd6rsfxShu88nDXOyR4xxjZC7nxAUncPA1ZBDqPd3D8SrnVd4iZC5WZAQZAElVgK6YkZAI52rUS1bIc0Lot2R3dQBMHdR5hdkVtFbgCbMnPCOlEAE6dSHZC5i4ZCASXOtXhZAlyMevlCNVZBhfXsvwZAHu8GhX0OupyDqRSPp708uCepeOhI8MmQ7ekMgWF1lM0mEnaS5ZC3fnz1BNYMQZD";
-const page_id="111758261888868";
+
+const storage = require('node-sessionstorage')
+ 
 
 const request = require("request"),
   express = require("express"),
   axios=require('axios'),
   body_parser = require("body-parser"),
-  app = express().use(body_parser.json());
+  app = express()
 const path=require('path')
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(express.json());
 app.listen(process.env.PORT || 2000, () => console.log("webhook is listening"));
 
 app.get('/',(req,res)=>{
   res.sendFile(path.join(__dirname + '/index.html'))
 })
-
+var user_token;
+var page_id;
+var access_token;
+app.post('/configure_webhook',(req,res)=>{
+  user_token=req.body.user_token
+  console.log(user_token)
+  var options = {
+    'method': 'GET',
+    'url': `https://graph.facebook.com/v16.0/me/accounts?access_token=${user_token}`,
+    'headers': {
+    }
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+        page_id=JSON.parse(response.body).data[0].id
+        access_token=JSON.parse(response.body).data[0].access_token
+  });
+})
 
 app.post("/webhook", async(req, res) => {
+  
+
+
+  
   let body = req.body;
   let recipient=body.entry[0].messaging[0].sender.id
   console.log(req.body)
@@ -23,14 +48,15 @@ app.post("/webhook", async(req, res) => {
     
     var options = {
       'method': 'POST',
-      'url': `https://graph.facebook.com/v16.0/${page_id}/messages?recipient={id: ${recipient}}&message={text: "hello"}&access_token=${token}`,
+      'url': `https://graph.facebook.com/v16.0/${page_id}/messages?recipient={id: ${recipient}}&message={text: "hello"}&access_token=${access_token}`,
       'headers': {
       }
     };
     request(options, function (error, response) {
       if (error) throw new Error(error);
       console.log(response.body);
-    });
+    }); 
+    res.end()
     
 
 });
