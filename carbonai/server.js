7184e54f13725674c06292bb2368ab377ec82bd1
YaKartoshka <erasyl.zarubekovv@gmail.com>
2024-01-17 16:33:29 +0600

    Update server.js

--- carbonai/server.js
+++ carbonai/server.js
@@ -3,34 +3,10 @@ const app = express();
 const Carbon = require('carbon-connect-js');
 const port = 4000;
 const apiKey = '66a2cb3c349b8abe34e87dff658ddc98ec7f55cdc49b11803ed48e75925c5307';
-var customerId = 'webapi'
-
-
-
-
-function getDocuments(con) {
-  // console.log(con.connections[0].synced_files);
-  // Carbon.getTextChunks({accessToken: accessTokenResponse, userFileId: con.connections[0].synced_files.external_file_id}).then((text)=>{
-  //   console.log(text)
-  // })
-  Carbon.getUserFiles({ accessToken: accessTokenResponse, environment: 'PRODUCTION' }).then((file) => {
-
-    Carbon.getTextChunks({ accessToken: accessTokenResponse, userFileId: file.data.results[1].id }).then((text) => {
-      console.log(text.data.results)
-    })
-  })
-}
-
-async function generateIntegrationOAuthURL() {
-
-}
-
-
 
 app.get('/', (req, res) => res.send('Hello World'));
 
 app.get('/carbon_cm_add', (req, res) => {
-
   var r = { r: 200 };
   var customerId = 123 // webapi user id
   Carbon.generateAccessToken({
@@ -41,7 +17,7 @@ app.get('/carbon_cm_add', (req, res) => {
     if (response.status == 200) {
       var accessTokenResponse = response.data.access_token; // keep it in database
       console.log('access token: ' + accessTokenResponse)
-      res.send(JSON.stringify(r))
+      res.send(JSON.stringify(accessTokenResponse));
     } else {
       accessTokenResponse = null;
       r['r'] = 0;
@@ -50,18 +26,21 @@ app.get('/carbon_cm_add', (req, res) => {
   });
 });
 
-app.get('/zendesk/auth', async (req, res) => {
+app.get('/integration/auth', async (req, res) => {
   var r = { r: 200 };
-  var access_token =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';   //req.body.access_token;
+  var access_token = req.query.access_token; 
+  var zendeskSubdomain = 'ailabssupport'
   try {
     const response = await Carbon.generateOauthurl({
       accessToken: access_token,
-      integrationName: 'ZENDESK',
+      integrationName: 'DROPBOX', 
+       // Accepted values are: NOTION, GOOGLE_DRIVE, ONEDRIVE, INTERCOM,
+       // DROPBOX, ZENDESK, BOX, CONFLUENCE, SHAREPOINT, ZOTERO
       optionalParams: {
-        zendeskSubdomain: 'ailabssupport'
+        zendeskSubdomain: zendeskSubdomain
       }
     });
-
+  
     if (response.status === 200) {
       res.redirect(response.data.oauth_url);
     } else {
@@ -74,11 +53,28 @@ app.get('/zendesk/auth', async (req, res) => {
   }
 })
 
-app.get('/get_user_connections', (req,res)=>{
-  var access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdhbml6YXRpb25faWQiOjEyOTEsImN1c3RvbWVyX2lkIjoxMjkxLCJvcmdhbml6YXRpb25fc3VwcGxpZWRfdXNlcl9pZCI6IjEyMyIsIm9yZ2FuaXphdGlvbl91c2VyX2lkIjozNjY1NSwiZXhwIjoxNzA1MzM3ODMyfQ.Bv1Zw9ZZzGKBj-dq-qR6hrM65FTxIpemZU4D8IuZu-k'
+app.get('/get_user_connections', (req, res) => {
+  var r = { r: 200 };
+  var access_token = req.query.access_token;
   Carbon.getUserConnections({ accessToken: access_token, environment: 'PRODUCTION' }).then((connections) => {
-      console.log(connections.connections)
+    console.log(connections.connections);
+    res.send(JSON.stringify(r));
+  });
+});
+
+app.get('/get_text', (req,res)=>{
+  var r = { r: 200 };
+  var access_token = req.query.access_token;
+  var user_file_id = '' // file id
+  
+  Carbon.getUserFiles({ accessToken: access_token, environment: 'PRODUCTION' }).then((file) => {
+    console.log(file.data.results)
+    Carbon.getTextChunks({ accessToken: access_token, userFileId: file.data.results[0].id }).then((text) => {
+      console.log(text.data.results);
+      res.send(JSON.stringify(r));
+    })
   })
+
 })
 
 
